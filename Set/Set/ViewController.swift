8984b05f02c5549d2dd61897052ec7e6dfdfc2ef
Aditya <aditya.s.bhat@gmail.com>
2017-12-03 20:57:51 -0800

    Project 2 Required Task 1: initial Set commit

---
+++ Set/Set/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Aditya Bhat on 12/3/17.
+//  Copyright © 2017 Aditya Bhat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
